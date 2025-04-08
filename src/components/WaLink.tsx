04b152df6a2ac3d5e4b1d1088dee4312de5a963a
Arfa Banyu Santoro <banyuarfa1122@gmail.com>
2025-04-08 17:36:05 +0700

    walink

--- src/components/WaLink.tsx
+++ src/components/WaLink.tsx
@@ -9,7 +9,7 @@ const WaLink = () => {
       href='https://wa.me/6287887708006?text=Halo%20admin,%20saya%20ingin%20menanyakan%20layanan%20perbaikan%20TV.%20Bisa%20dibantu?'
       className='btn btn-primary btn-lg'
       onClick={() =>
-        sendGAEvent('event', 'Whatsapp', { value: 'tombol wa diklik' })
+        sendGAEvent('event', 'click_whatsapp', { value: 'Hubungi' })
       }
     >
       Hubungi <FaArrowRight />
